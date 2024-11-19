33e6531a086d8bfc4fba70d563ac8b8ce82ac939
Dynosol <sol.evergreen@gmail.com>
2024-11-19 04:14:44 -0500

    next time, fix import_courses, fix the styling for contact page, set up professor list models and dataframes, attach that to course dataframes, etc

--- qguideguide_backend/courses/management/commands/import_courses.py
+++ qguideguide_backend/courses/management/commands/import_courses.py
@@ -65,6 +65,7 @@ class Command(BaseCommand):
                         'students_enrolled': course_data.get('Students Enrolled'),
                         'response_count': course_data.get('Student Response Count'),
                         'response_rate': self.clean_percentage(course_data.get('Response Rate %')),
+                        'overall_score': self.clean_numeric_value(course_data.get('Feedback', {}).get('course_general_questions', [{}])[0].get('Course Mean')),
                     }
                 )
 
@@ -75,42 +76,37 @@ class Command(BaseCommand):
                 feedback_data = course_data.get('Feedback', {})
                 
                 # Table_2 typically contains the overall course ratings
-                if 'Table_2' in feedback_data and feedback_data['Table_2']:
-                    table_2 = feedback_data['Table_2'][0]  # Get first entry
+                if 'course_general_questions' in feedback_data and feedback_data['course_general_questions']:
+                    course_general_questions = feedback_data['course_general_questions'][0]  # Get first entry
                     
                     defaults = {
-                        'excellent_count': self.clean_numeric_value(table_2.get('Excellent')) or 0,
-                        'very_good_count': self.clean_numeric_value(table_2.get('Very Good')) or 0,
-                        'good_count': self.clean_numeric_value(table_2.get('Good')) or 0,
-                        'fair_count': self.clean_numeric_value(table_2.get('Fair')) or 0,
-                        'unsatisfactory_count': self.clean_numeric_value(table_2.get('Unsatisfactory')) or 0,
-                        'course_mean': self.clean_numeric_value(table_2.get('Course Mean')),
-                        'fas_mean': self.clean_numeric_value(table_2.get('FAS Mean')),
-                        'instructor_mean': self.clean_numeric_value(table_2.get('Instructor Mean')),
+                        'excellent_count': self.clean_numeric_value(course_general_questions.get('Excellent')) or 0,
+                        'very_good_count': self.clean_numeric_value(course_general_questions.get('Very Good')) or 0,
+                        'good_count': self.clean_numeric_value(course_general_questions.get('Good')) or 0,
+                        'fair_count': self.clean_numeric_value(course_general_questions.get('Fair')) or 0,
+                        'unsatisfactory_count': self.clean_numeric_value(course_general_questions.get('Unsatisfactory')) or 0,
+                        'course_mean': self.clean_numeric_value(course_general_questions.get('Course Mean')),
+                        'fas_mean': self.clean_numeric_value(course_general_questions.get('FAS Mean')),
+                        'instructor_mean': self.clean_numeric_value(course_general_questions.get('Instructor Mean')),
                     }
                     
-                    rating_breakdown, created = CourseRatingBreakdown.objects.update_or_create(
+                    CourseRatingBreakdown.objects.update_or_create(
                         course=course,
                         defaults=defaults
                     )
                     
-                    if created:
-                        ratings_created += 1
-
-                    # Update course overall score from Table_2
-                    course.overall_score = self.clean_numeric_value(table_2.get('Course Mean'))
-                    course.save()
+                    ratings_created += 1
 
                 # Process comments if they exist (usually in Table_3)
-                if 'Table_3' in feedback_data:
-                    for entry in feedback_data['Table_3']:
+                if 'comments_from_students' in feedback_data:
+                    for entry in feedback_data['comments_from_students']:
                         if 'Value' in entry and entry['Value']:
-                            comment, created = CourseComment.objects.get_or_create(
+                            CourseComment.objects.update_or_create(
                                 course=course,
-                                comment_text=entry['Value']
+                                comment_text=entry['Value'],
+                                defaults={}
                             )
-                            if created:
-                                comments_created += 1
+                            comments_created += 1
 
                 if courses_created % 10 == 0:
                     self.stdout.write(f'Processed {courses_created} courses...')
@@ -125,6 +121,6 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS(f'''
 Import completed successfully:
 - Courses created/updated: {courses_created}
-- Rating breakdowns created: {ratings_created}
-- Comments created: {comments_created}
+- Rating breakdowns created/updated: {ratings_created}
+- Comments created/updated: {comments_created}
 '''))
