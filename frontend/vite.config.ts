19fc7bb19fa769264b4a6df496bb184ed691188d
Dynosol <sol.evergreen@gmail.com>
2025-01-23 03:41:43 -0500

    more changes to render yaml

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: 'assets',
+  },
+  base: '/',
 })
