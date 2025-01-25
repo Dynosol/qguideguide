e9ff0f6ea3db30c53ae165a799c647ef809e6001
Dynosol <sol.evergreen@gmail.com>
2025-01-24 20:19:07 -0500

    Remove files that should be ignored from git tracking

--- backend/staticfiles/js/nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const logo = document.getElementById('logo');
-    if (logo) {
-        logo.addEventListener('click', function () {
-            const url = logo.getAttribute('data-url');
-            window.location.href = url;
-        });
-    }
-});
