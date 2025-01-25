922b367c1dcfbf3017f5f3a680a00ddfb2d7cf6c
Dynosol <sol.evergreen@gmail.com>
2025-01-24 20:27:18 -0500

    including processing departments?

--- backend/professors/serializers.py
+++ backend/professors/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from .models import Professor, Department
 
 class DepartmentSerializer(serializers.ModelSerializer):
+    professor_count = serializers.IntegerField(required=False)
+    
     class Meta:
         model = Department
-        fields = '__all__'
+        fields = ['id', 'name', 'empirical_bayes_average', 'empirical_bayes_rank', 'professor_count']
 
 class ProfessorSerializer(serializers.ModelSerializer):
     class Meta:
